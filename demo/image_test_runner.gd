860715528db478457efb8cfe19fa18ccd40d1da1
FrancoYudica <francoyudica@gmail.com>
2026-04-06 21:29:03 -0300

    Added rotation script

--- demo/image_test_runner.gd
+++ demo/image_test_runner.gd
@@ -12,7 +12,7 @@ func _ready() -> void:
 	engine.init()
 	
 	# Pre-register the model once
-	model_id = engine.register_model("ml/sepia_mlp.onnx")
+	model_id = engine.register_model("ml/invert_mlp.onnx")
 	
 	# Initialize the result texture once to avoid allocation stutters
 	result_texture = ImageTexture.new()
