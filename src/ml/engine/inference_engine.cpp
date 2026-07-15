ea5e35a549648d97c664c4a981ed5c43fd44e9e5
FrancoYudica <francoyudica@gmail.com>
2026-07-15 19:47:37 -0300

    Graph emplace bug fix

--- src/ml/engine/inference_engine.cpp
+++ src/ml/engine/inference_engine.cpp
@@ -126,13 +126,12 @@ uint32_t MLInferenceEngine::register_model(Ref<ONNXResource> resource) {
     graph_context.initializers_tm.instantiate();
     graph_context.initializers_tm->init(_rd, &_sb_pool);
 
-    uint32_t graph_rid = _next_graph_id++;
-    _graphs.emplace(graph_rid, std::move(graph_context));
-
     for (const auto& [name, tensor] : graph_context.graph.initializers) {
         graph_context.initializers_tm->get_or_create(name, tensor.shape, tensor.data);
     }
 
+    uint32_t graph_rid = _next_graph_id++;
+    _graphs.emplace(graph_rid, std::move(graph_context));
     return graph_rid;
 }
 
